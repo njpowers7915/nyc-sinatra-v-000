08967d68a80ee910e779627ebd1f1deea5ffab1b
Nicholas Powers <njpowers7915@gmail.com>
2018-07-05 23:38:45 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -19,9 +19,33 @@ class FiguresController < ApplicationController
         erb :'figures/edit'
     end
 
-    post '/figures/new' do
-        @figure = Figure.new(params)
+    post '/figures' do
+        @figure = Figure.create(params["figure"])
+        if !params[:landmark][:name].empty?
+            @figure.landmarks << Landmark.create(params[:landmark])
+        end
+
+        if !params[:title][:name].empty?
+            @figure.titles << Title.create(params[:title])
+        end
+
         @figure.save
-        redirect '/figures'
+        redirect "/figures/#{@figure.id}"
+    end
+
+    post '/figures/:id' do
+        @figure = Figure.find(params[:id])
+        @figure.update(params[:figure])
+
+        if !params[:landmark][:name].empty?
+            @figure.landmarks << Landmark.create(params[:landmark])
+        end
+
+        if !params[:title][:name].empty?
+            @figure.titles << Title.create(params[:title])
+        end
+
+        @figure.save
+        redirect to "/figures/#{@figure.id}"
     end
 end
