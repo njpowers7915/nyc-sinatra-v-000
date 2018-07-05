04b133a039b6614ae70d97078737874d92399465
Nicholas Powers <njpowers7915@gmail.com>
2018-07-05 23:33:47 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,12 +1,17 @@
 class FiguresController < ApplicationController
 
     get '/figures' do
+        @figures = Figure.all
         erb :'figures/index'
     end
 
     get '/figures/new' do
         erb :'figures/new'
     end
+
+    get '/figures/:id' do
+        @figure = Figure.find(params[:id])
+        erb :'figures/show'
 
     post '/figures/new' do
         @figure = Figure.new(params)
